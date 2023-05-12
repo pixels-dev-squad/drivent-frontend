d9a5f03e15d169f5ac94dff11702564727d453c8
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-05-11 21:59:45 -0300

    Almost finish feat/hotels

--- src/pages/Dashboard/Hotel/HotelContainer.js
+++ src/pages/Dashboard/Hotel/HotelContainer.js
@@ -1,21 +1,64 @@
 import styled from 'styled-components';
-
+import { useState } from 'react';
 export default function HotelContainer(props) {
-  //eslint-disable-next-line
   console.log(props);
+  const [clicked, setClicked] = useState(false);
   return (
-    <HotelContainerStyled>
-      <img src={props.image} width={'168px'} height={'109px'}></img>
-      <h1>{props.name}</h1>
+    <HotelContainerStyled onClick={() => setClicked(!clicked)} clicked={clicked}>
+      <ImageContainerStyled>
+        <img src={props.image} width={'168px'} height={'109px'}></img>
+      </ImageContainerStyled>
+
+      <DescriptionContainerStyled>
+        <h1>{props.name}</h1>
+        <InformationContainerStyled>
+          <h1>Tipos de acomodação</h1>
+          <h2>
+            {props.acomodationType.slice(0, -1).join(', ')} e {props.acomodationType.slice(-1)}
+          </h2>
+        </InformationContainerStyled>
+        <InformationContainerStyled>
+          <h1>Vagas disponíveis</h1>
+          <h2>{props.capacity}</h2>
+        </InformationContainerStyled>
+      </DescriptionContainerStyled>
     </HotelContainerStyled>
   );
 }
 
 const HotelContainerStyled = styled.div`
-    width: 196px;
-    height: 264px;
-    background: #ebebeb;
-    border-radius: 10px;
-    box-sizing: border-box;
-    padding: 14px;
+  width: 196px;
+  height: 264px;
+  background: ${(props) => (!props.clicked ? '#ebebeb' : '#FFEED2')};
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 14px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const DescriptionContainerStyled = styled.div`
+  width: 100%;
+  height: 117px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  p {
+  }
+`;
+
+const ImageContainerStyled = styled.div`
+  width: 168px;
+  height: 109px;
+`;
+
+const InformationContainerStyled = styled.div`
+  width: 100%;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 14px;
+  h2 {
+    font-weight: 400;
+  }
 `;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -15,24 +15,51 @@ export default function Hotel() {
       const hotel = await getHotels(token);
       const promisses = hotel.map((h) => getHotelById(token, h.id));
       const newHotelsWithRooms = await Promise.all(promisses);
+      let capacity = 0;
+      let acomodationType = [];
+
+      newHotelsWithRooms.map((h) => {
+        console.log(h);
+        h.Rooms.map((r) => {
+          console.log(r);
+          if (r.capacity === 3 && !acomodationType.includes('Triple')) {
+            acomodationType.push('Triple');
+          }
+          if (r.capacity === 2 && !acomodationType.includes('Double')) {
+            acomodationType.push('Double');
+          }
+          if (r.capacity === 1 && !acomodationType.includes('Single')) {
+            acomodationType.push('Single');
+          }
+          capacity += r.capacity;
+        });
+
+        h['capacity'] = capacity;
+        h['acomodationType'] = acomodationType;
+      });
       setHotelsWithRooms(newHotelsWithRooms);
     }
 
     fetchHotels();
   }, [token]);
-
+  console.log(hotelsWithRooms);
   //eslint-disable-next-line
-  hotelsWithRooms.map((h) =>
-    //eslint-disable-next-line
-    console.log(h.image)
-  );
+  //eslint-disable-next-line
   return (
     <>
       <Title>Escolha de hotel e quarto</Title>
       <Subtitle show={true}>Primeiro, escolha seu hotel</Subtitle>
       <HotelsContainerStyled>
         {hotelsWithRooms.map((h) => (
-          <HotelContainer image={h.image} name={h.name} key={h.id} />
+          <HotelContainer
+            image={h.image}
+            name={h.name}
+            key={h.id}
+            capacity={h.capacity}
+            acomodationType={h.acomodationType}
+            hotelId={h.id}
+            rooms={h.Rooms}
+          />
         ))}
       </HotelsContainerStyled>
     </>
@@ -42,6 +69,7 @@ export default function Hotel() {
 const HotelsContainerStyled = styled.div`
   display: flex;
   justify-content: start;
+  gap: 10px;
 `;
 
 //eslint-disable-next-line
